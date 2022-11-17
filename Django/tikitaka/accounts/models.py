d3dce2df1cd6fe9fbb4410e6605cc499104a1288
seoda0000 <seoda0000@gmail.com>
2022-11-17 16:05:55 +0900

    살려주세요

--- Django/tikitaka/accounts/models.py
+++ Django/tikitaka/accounts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
 
 
 # Create your models here.
@@ -10,4 +13,11 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    
+
+    def __str__(self):
+        return self.email
 
