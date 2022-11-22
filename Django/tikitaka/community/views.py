0a37cd4ff88a5f2477787d707fc32af4f29493a0
seoda0000 <seoda0000@gmail.com>
2022-11-22 13:45:49 +0900

    review delete

--- Django/tikitaka/community/views.py
+++ Django/tikitaka/community/views.py
@@ -136,8 +136,9 @@ def review_detail(request, review_pk):
             serializer.save()
             return Response(serializer.data)
     elif request.method == 'DELETE':
+        username = review.user.username
         review.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(username)
 
 
 
